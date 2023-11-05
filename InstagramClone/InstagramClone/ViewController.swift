a6906c49f6f9983d1d882cd098d767f4100edbed
FurkanMDemiray <furkanmdemiray@gmail.com>
2023-11-05 23:20:23 +0300

    Added
    TabBar control
    Firebase with swift package

--- InstagramClone/InstagramClone/ViewController.swift
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var mailField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func signInBtn(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toTabBarVC", sender: nil)
+    }
 
+    @IBAction func signUpBtn(_ sender: Any) {
+    }
 }
 
