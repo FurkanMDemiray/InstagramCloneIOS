f3946e3623d115aef096d4a3dac115fcf5c7657d
FurkanMDemiray <furkanmdemiray@gmail.com>
2023-11-08 00:01:17 +0300

    added
    Upload imageView function

--- InstagramClone/InstagramClone/UploadViewController.swift
+++ InstagramClone/InstagramClone/UploadViewController.swift
@@ -6,24 +6,77 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameField: UITextField!
+
+    @IBOutlet weak var uploadBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
+        imageView.isUserInteractionEnabled = true
+
+
+        let imageGesture = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(imageGesture)
+
+
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func uploadBtnAction(_ sender: Any) {
+
+        print("tıklandı upload button")
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+
+        let mediaFolder = storageRef.child("media")
+
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+
+            let imageReference = mediaFolder.child("image.jpg")
+            imageReference.putData(data) { metaData, error in
+
+                if let err = error {
+                    print(err.localizedDescription)
+                } else {
+                    imageReference.downloadURL { url, error in
+
+                        if error == nil {
+                            if let imageUrl = url?.absoluteString {
+                                print(imageUrl)
+                            }
+
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+
+
+
+
     }
-    */
+    @objc func chooseImage() {
 
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+
+    }
+
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
 }
