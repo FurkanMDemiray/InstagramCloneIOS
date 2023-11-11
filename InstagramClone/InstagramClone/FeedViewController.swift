ab4ab1f6dfc81bc3a7e547a1cbe8837c7f4123b9
FurkanMDemiray <furkanmdemiray@gmail.com>
2023-11-11 18:00:43 +0300

    Added
    GetData method
    GetData from Firestorage

--- InstagramClone/InstagramClone/FeedViewController.swift
+++ InstagramClone/InstagramClone/FeedViewController.swift
@@ -6,10 +6,17 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var feedTableView: UITableView!
+
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,23 +25,82 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         feedTableView.delegate = self
         feedTableView.dataSource = self
 
+        getData()
     }
 
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
 
-        cell.commentLabel.text = "comment"
-        cell.likeLabel.text = "0"
-        cell.userImageView.image = UIImage(named: "Homer.png")
-        cell.userMail.text = "deneme@gmail.com"
+        cell.commentLabel.text = userCommentArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.userImageView.image = UIImage(named: "user.png")
+        cell.userMail.text = userEmailArray[indexPath.row]
 
         return cell
+    }
+
+    func getData() {
+
+        let firebaseStorage = Firestore.firestore()
+
+
+        firebaseStorage.collection("Posts").addSnapshotListener { snapShot, error in
+
+            if let err = error {
+
+                self.makeAlert(title: "Error", error: err.localizedDescription)
+            }
+            else {
+
+                if snapShot?.isEmpty == false {
+
+                    for document in snapShot!.documents {
+
+                        let id = document.documentID
+
+                        if let postedBy = document.get("postedBy") as? String {
+                            print(postedBy)
+                            self.userEmailArray.append(postedBy)
+                        }
+                        if let postComment = document.get("postComment") as? String {
+                            print(postComment)
+                            self.userCommentArray.append(postComment)
+                        }
+                        if let likes = document.get("likes") as? Int {
+                            print(likes)
+                            self.likeArray.append(likes)
+                        }
+                        if let imageUrl = document.get("imageUrl") as? String {
+                            print(imageUrl)
+                            let url = URL(string: imageUrl)
+                            self.userImageArray.append(imageUrl)
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+
+    }
+
+
+    func makeAlert(title: String, error: String) {
+
+        let alert = UIAlertController(title: title, message: error, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+
+        alert.addAction(okButton)
+
+        self.present(alert, animated: true, completion: nil)
+
     }
 
 }
